03b746e8c2a5c01452ae023fee39bb1024ba609d
Shourov <shourov.ahmed@student.ap.be>
2021-03-25 00:04:29 +0100

    Added NavBar component

---
+++ SQM-Website/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+declare const Menu: any;
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
