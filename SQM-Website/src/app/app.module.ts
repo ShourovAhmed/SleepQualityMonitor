8cb160b8554dceb6f339d7ad24ff2740e9aa6231
Shourov <shourov.ahmed@student.ap.be>
2021-03-25 00:10:34 +0100

    Added tips, notes and graph components

--- SQM-Website/src/app/app.module.ts
+++ SQM-Website/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { GraphComponent } from './graph/graph.component';
+import { NotesComponent } from './notes/notes.component';
+import { TipsComponent } from './tips/tips.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    GraphComponent,
+    NotesComponent,
+    TipsComponent
   ],
   imports: [
     BrowserModule
