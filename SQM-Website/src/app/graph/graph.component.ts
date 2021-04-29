95de5f6e03f9e7060725d3d75e6772d0e2162fde
Jonathan <tayarjonathan2@gmail.com>
2021-04-29 10:22:41 +0200

    average BPM

--- SQM-Website/src/app/graph/graph.component.ts
+++ SQM-Website/src/app/graph/graph.component.ts
@@ -42,6 +42,16 @@ export class GraphComponent{
   //Deze data komt van de HRM en wordt uit de database gehaald, wanneer dit uiteindelijk lukt
   DB_Timestamps = ["05:58", "06:03", "06:08", "06:13", "06:18", "06:23", "06:28", "06:33", "06:38", "06:43", "06:48", "06:53", "06:58", "07:03", "07:08", "07:13", "07:18", "07:23", "07:28", "07:33", "07:38", "07:43", "07:48", "07:53", "07:58"]
   DB_BPM = [59, 63, 64, 62, 61, 65, 57, 61, 65, 58, 61, 58, 54, 56, 53, 60, 55, 57, 58, 47, 50, 57, 54, 68, 81]
+ // avg_BPM :number = som/DB_BPM.length;
+  
+  public get avg_BPM(){
+    let som = 0
+    for (let index = 0; index < this.DB_BPM.length; index++) {
+      som += this.DB_BPM[index];
+    }
+    return som/this.DB_BPM.length
+  } 
+  
 
   constructor() {
     this.chartOptions = {
@@ -74,7 +84,7 @@ export class GraphComponent{
         curve: "smooth"
       },
       title: {
-        text: "Average Heart Rate",
+        text: "Average Heart Rate: " + this.avg_BPM,
         align: "left"
       },
       grid: {
