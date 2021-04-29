61aae481e650b7c4bfa29c49312e8ae21363fe9b
Shourov <shourov.ahmed@student.ap.be>
2021-04-29 09:26:32 +0200

    Added notes

--- SQM-Website/src/app/notes/notes.component.ts
+++ SQM-Website/src/app/notes/notes.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesComponent implements OnInit {
 
+  lijst: resultaat[] = [];
+
+  note = "";
+  date = "27/04/2021";
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  AddNote(note: string, date : string){
+    console.log("AddNote" + note + date)
+    this.note = note;
+
+    this.lijst.push(new resultaat(note,this.date))
+   
+    console.log(this.lijst)    
+  }
+
+}
+
+class resultaat {
+  note: string;
+  date: string;
+  constructor(note: string , date: string) {
+    console.log("constructor" + note + date)
+    this.note = note;
+    this.date = date;
+  }
 }
